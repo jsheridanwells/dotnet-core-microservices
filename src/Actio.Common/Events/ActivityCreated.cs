3ae0b2bdc99020f7733182def001018e1156a89a
Jeremy Wells <jeremywells@Jeremys-MacBook-Pro.local>
2018-11-11 14:55:10 -0600

    created message events

---
+++ src/Actio.Common/Events/ActivityCreated.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Actio.Common.Events
+{
+    public class ActivityCreated : IAuthenticatedEvent
+    {
+
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; }
+
+        protected ActivityCreated()
+        {
+        }
+
+        public ActivityCreated(Guid id, Guid userId, string category, string name, string description, DateTime createdAt)
+        {
+            Id = id;
+            UserId = userId;
+            Category = category;
+            Name = name;
+            Description = description;
+            CreatedAt = createdAt;
+        }
+    }
+}
